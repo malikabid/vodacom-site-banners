d07beff07e31ef6da8d4499579a1fb4cb670818e
Abid Hussain Malik <malikabid2006@gmail.com>
2025-11-28 19:52:44 +0530

    Fix namespace declaration in View controller for Vodacom Site Banners

--- Controller/Index/View.php
+++ Controller/Index/View.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Vendor\SiteBanners\Controller\Index;
+namespace Vodacom\SiteBanners\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\ResultFactory;
